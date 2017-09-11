655b4a83388068e5c3de0e10b7a6c4fc291453da
Artur de Sousa Rocha <adsr@poczta.onet.pl>
2017-09-11 23:35:17 +0200

    Smarter use of text tags

--- unjabberlib/tkui.py
+++ unjabberlib/tkui.py
@@ -1,4 +1,5 @@
 from tkinter import *
+from tkinter.scrolledtext import ScrolledText
 
 
 class UnjabberTk(Tk):
@@ -9,29 +10,18 @@ class UnjabberTk(Tk):
         self.who_var.trace_add('write', lambda *args: self.who())
         w = OptionMenu(self, self.who_var, *self.queries.who(None))
         w.pack()
-        self.text = Text(self)
+        self.text = ScrolledText(self)
         self.text.pack()
 
     def who(self):
         self.text.delete('1.0', END)
-        column = 0
-        row = 1
+        self.text.tag_configure("when", foreground='blue')
         messages = self.queries.messages_for_whom(self.who_var.get())
         for index, message in enumerate(messages):
             when = str(message.when)
-            self.text.insert(END, when)
+            self.text.insert(END, when, "when")
             self.text.insert(END, ' ')
             self.text.insert(END, message.shortname)
-            tag_name = 'when{}'.format(index)
-            self.text.tag_add(tag_name, position(row, column),
-                              position(row, column + len(when)))
-            self.text.tag_configure(tag_name, foreground='blue')
             self.text.insert(END, '\n')
-            row += 1
             self.text.insert(END, message.what)
             self.text.insert(END, '\n')
-            row += 1
-
-
-def position(row, column):
-    return '{}.{}'.format(row, column)
