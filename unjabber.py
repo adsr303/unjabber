61ce0d7db2bf9b9ba83761b5ac98a403bd5b6a18
Artur de Sousa Rocha <adsr@poczta.onet.pl>
2017-09-11 22:11:07 +0200

    Move cmd UI to library
    Preparing for Tk UI

--- unjabber.py
+++ unjabber.py
@@ -1,59 +1,10 @@
 #!/usr/bin/env python3
 
 import argparse
-import cmd
 import sqlite3
-from itertools import zip_longest
 
+from unjabberlib.cmdui import UnjabberCmd
 from unjabberlib.queries import Queries
-
-
-class Unjabber(cmd.Cmd):
-    def __init__(self, queries, **cmdargs):
-        super().__init__(**cmdargs)
-        self.queries = queries
-
-    def do_who(self, arg):
-        """Show list of people. Add part of a name to narrow down."""
-        for name in self.queries.who(arg):
-            print(name)
-
-    def do_show(self, arg):
-        """Show conversations with people matching name (or part of)."""
-        previous = None
-        for message in self.queries.messages_for_whom(arg):
-            print_message(previous, message)
-            previous = message
-
-    def do_grep(self, arg):
-        """Show messages containing text."""
-        for message in self.queries.grep(arg):
-            print(message)
-
-    def do_quit(self, arg):
-        """Exit the program."""
-        return True
-
-
-def print_message(previous, message):
-    day, hour, shortname = message.after(previous)
-    if day:
-        if previous:
-            print()
-        print('==', day, '==')
-    if shortname:
-        if not day:
-            print()
-        print(hour, ' -- ', shortname)
-        sub_print_message(5 * ' ', message.what)
-    else:
-        sub_print_message(hour if hour else 5 * ' ', message.what)
-
-
-def sub_print_message(hour, what):
-    for h, line in zip_longest([hour], what.split('\n'), fillvalue=5 * ' '):
-        print(h, line)
-
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='Read Cisco Jabber database')
@@ -61,4 +12,4 @@ if __name__ == '__main__':
     args = parser.parse_args()
 
     with sqlite3.connect(args.database) as con:
-        Unjabber(Queries(con)).cmdloop()
+        UnjabberCmd(Queries(con)).cmdloop()
---
+++ unjabberlib/cmdui.py
@@ -0,0 +1,49 @@
+import cmd
+from itertools import zip_longest
+
+
+class UnjabberCmd(cmd.Cmd):
+    def __init__(self, queries, **cmdargs):
+        super().__init__(**cmdargs)
+        self.queries = queries
+
+    def do_who(self, arg):
+        """Show list of people. Add part of a name to narrow down."""
+        for name in self.queries.who(arg):
+            print(name)
+
+    def do_show(self, arg):
+        """Show conversations with people matching name (or part of)."""
+        previous = None
+        for message in self.queries.messages_for_whom(arg):
+            print_message(previous, message)
+            previous = message
+
+    def do_grep(self, arg):
+        """Show messages containing text."""
+        for message in self.queries.grep(arg):
+            print(message)
+
+    def do_quit(self, arg):
+        """Exit the program."""
+        return True
+
+
+def print_message(previous, message):
+    day, hour, shortname = message.after(previous)
+    if day:
+        if previous:
+            print()
+        print('==', day, '==')
+    if shortname:
+        if not day:
+            print()
+        print(hour, ' -- ', shortname)
+        sub_print_message(5 * ' ', message.what)
+    else:
+        sub_print_message(hour if hour else 5 * ' ', message.what)
+
+
+def sub_print_message(hour, what):
+    for h, line in zip_longest([hour], what.split('\n'), fillvalue=5 * ' '):
+        print(h, line)
